b938881a5831465d384417eb8a038b692c2f9e2b
Aishwarya Ram Solaisamy Ramachandran <aishwaryaramsr16@users.noreply.github.com>
2021-01-31 11:49:30 +0000

    Added cart reducers

--- app/reducers/cart.js
+++ app/reducers/cart.js
@@ -1,20 +1,51 @@
 import {
     CART_LOADED,
-} from '../actions/cart';
-
-function cartReducer(state={ items: {} }, action) {
-
-    switch(action.type) {
-        case CART_LOADED:
-            return {
-                ...state, 
-                id: action.cartId,
-                items: action.cartItems,
-            }
-        default:
-            return state;
+    ADD_TO_CART,
+    DECRE_QTY,
+    INCRE_QTY,
+    DELETE_FROM_CART,
+    CLEAR_CART,
+  } from "../actions/cart";
+  
+  function cartReducer(state = { items: {} }, action) {
+    switch (action.type) {
+      case CART_LOADED:
+        return {
+          ...state,
+          id: action.cartId,
+          items: action.cartItems,
+        };
+  
+      case CLEAR_CART:
+        return { ...state, items: {} };
+      case ADD_TO_CART:
+        return {
+          ...state,
+          items: { ...state.items, ...action.payload.cartItems },
+        };
+  
+      case DECRE_QTY:
+        return {
+          ...state,
+          items: { ...state.items, ...action.payload.cartItems },
+        };
+      case INCRE_QTY:
+        return {
+          ...state,
+          items: { ...state.items, ...action.payload.cartItems },
+        };
+      case DELETE_FROM_CART: {
+        let newItems = Object.assign({}, state.items);
+        delete newItems[action.payload];
+        return {
+          ...state,
+          items: { ...newItems },
+        };
+      }
+      default:
+        return state;
     }
-
-}
-
-export default cartReducer;
+  }
+  
+  export default cartReducer;
+  
