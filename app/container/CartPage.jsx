e8e8b6ca0809927540cd36e63d04d0fcef900d71
Aishwarya Ram Solaisamy Ramachandran <aishwaryaramsr16@users.noreply.github.com>
2021-01-31 11:54:32 +0000

    converted from class to functional component

--- app/container/CartPage.jsx
+++ app/container/CartPage.jsx
@@ -1,49 +1,87 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { ProductList } from '../components';
+import { ProductList, CartList } from "../components";
 
-import * as cartActions from '../actions/cart';
+import {
+  loadCart,
+  addToCart,
+  deleteFromCart,
+  incrementQty,
+  decrementQty,
+  clearCart,
+} from "../actions/cart";
 
-export class CartPage extends Component {
+export default function CartPage() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+  const cart = useSelector((state) => state.cart);
+  const { id } = cart;
 
-    constructor(props) {
-        super(props);
-        props.loadCart();
-    }
+  useEffect(() => {
+    dispatch(loadCart());
+  }, [loadCart, dispatch]);
 
-    render() {
-        const { isLoading, products, cart } = this.props;
+  const addToCartHandler = (id) => {
+    dispatch(addToCart(id));
+  };
 
-        if (isLoading) {
-            return <div> Loading your cart </div>;
-        }
+  const incrementQtyHandler = (id) => {
+    dispatch(incrementQty(id));
+  };
+  const decrementQtyHandler = (id) => {
+    dispatch(decrementQty(id));
+  };
 
-        return (
-            <div className="container">
-                <header className="mt-5 mb-5">
-                    <h1>Shopping Cart</h1>
-                </header>
-                <main className="row">
-                    <section className="col">
-                        <ProductList products={products} addToCart={() => {}} />
-                    </section>
-                    <section className="col">
-                        {/* Cart */}
-                    </section>
-                </main>
-            </div>
-        )
-    }
+  const deleteFromCartHandler = (id) => {
+    dispatch(deleteFromCart(id));
+  };
 
+  const clearCartHandler = () => {
+    dispatch(clearCart());
+  };
+
+  const totalCartAmount = () => {
+    let sum = 0;
+    Object.values(cart.items).forEach((cartItem) => {
+      sum += products[cartItem.itemId - 1].price * cartItem.quantity;
+    });
+    return sum;
+  };
+
+  if (!id) {
+    return <div> Loading your cart </div>;
+  }
+  return (
+    <div className="container">
+      <header className="mt-5 mb-5">
+        <h1>Shopping Cart</h1>
+      </header>
+      <main className="row">
+        <section className="col">
+          <ProductList products={products} addToCart={addToCartHandler} />
+        </section>
+        <section className="col">
+          <CartList
+            cart={cart}
+            products={products}
+            incrementQty={incrementQtyHandler}
+            decrementQty={decrementQtyHandler}
+            deleteItem={deleteFromCartHandler}
+          />
+        </section>
+      </main>
+      <br />
+      <span className="float-right">Total £ {totalCartAmount()}</span>
+      <br />
+      <br />
+      <button
+        className="btn btn-outline-primary btn-sm  float-right"
+        type="button"
+        onClick={() => clearCartHandler()}
+      >
+        Clear Cart
+      </button>
+    </div>
+  );
 }
-
-export function mapStateToProps({ cart, products }) {
-    return {
-        isLoading: !cart.id,
-        products,
-        cart,
-    }
-}
-
-export default connect(mapStateToProps, cartActions)(CartPage);
