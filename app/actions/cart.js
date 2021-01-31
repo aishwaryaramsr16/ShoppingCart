f54ed920c71a28804690ee3ba504f51a6321fd2b
Aishwarya Ram Solaisamy Ramachandran <aishwaryaramsr16@users.noreply.github.com>
2021-01-31 11:48:23 +0000

    Added cart actions

--- app/actions/cart.js
+++ app/actions/cart.js
@@ -8,19 +8,83 @@ export const DECRE_QTY = "DECREMENT_QTY";
 export const DELETE_FROM_CART = "DELETE_FROM_CART";
 export const CLEAR_CART = "CLEAR_CART";
 
-
-
-
 export function loadCart() {
-    return async (dispatch, _, config) => {
-        dispatch({ type: CART_LOADING });
+  return async (dispatch, _, config) => {
+    dispatch({ type: CART_LOADING });
 
-        const response = await fetch(config.cartApi, { method: 'POST', body: JSON.stringify({}) });
-        const cart = await response.json();
+    const response = await fetch(config.cartApi, {
+      method: "POST",
+      body: JSON.stringify({}),
+    });
+    const cart = await response.json();
 
-        dispatch({
-            type: CART_LOADED,
-            ...cart,
-        })
-    };
+    dispatch({
+      type: CART_LOADED,
+      ...cart,
+    });
+  };
+}
+
+export function addToCart(id) {
+  return async (dispatch, getState, config) => {
+    const { data } = await axios.post(
+      config.cartApi + `/${getState().cart.id}/item/${id}`,
+      { quantity: 1 }
+    );
+    dispatch({
+      type: ADD_TO_CART,
+      payload: {
+        ...data,
+      },
+    });
+  };
+}
+
+export function incrementQty(id) {
+  return async (dispatch, getState, config) => {
+    const { data } = await axios.post(
+      config.cartApi + `/${getState().cart.id}/item/${id}/increment`
+    );
+
+    dispatch({
+      type: INCRE_QTY,
+      payload: {
+        ...data,
+      },
+    });
+  };
+}
+
+export function decrementQty(id) {
+  return async (dispatch, getState, config) => {
+    const { data } = await axios.post(
+      config.cartApi + `/${getState().cart.id}/item/${id}/decrement`
+    );
+
+    dispatch({
+      type: DECRE_QTY,
+      payload: {
+        ...data,
+      },
+    });
+  };
+}
+
+export function deleteFromCart(id) {
+  return async (dispatch, getState, config) => {
+    await axios.delete(config.cartApi + `/${getState().cart.id}/item/${id}`);
+    dispatch({
+      type: DELETE_FROM_CART,
+      payload: id,
+    });
+  };
+}
+
+export function clearCart() {
+  return async (dispatch, getState, config) => {
+    await axios.post(config.cartApi + `/${getState().cart.id}/clear`);
+    dispatch({
+      type: CLEAR_CART,
+    });
+  };
 }
