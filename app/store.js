23bbf12682aa17d7aadc24fc76ed6c1410eaed5b
Aishwarya Ram Solaisamy Ramachandran <aishwaryaramsr16@users.noreply.github.com>
2021-01-31 11:50:20 +0000

    Added redux composed with dev tools

--- app/store.js
+++ app/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const config = {
-    cartApi: 'http://localhost:4567/cart'
+  cartApi: "http://localhost:4567/cart",
 };
 
 const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk.withExtraArgument(config))
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(config)))
 );
 
 export default store;
